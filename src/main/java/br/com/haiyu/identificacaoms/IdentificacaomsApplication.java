3bde8557a6f5e704638b0b3ffaeeb936ae14f97e
Marcola <marcosguimaraest04@gmail.com>
2022-12-23 11:48:49 -0300

    Iniciado projeto Spring Boot no repositório

---
+++ src/main/java/br/com/haiyu/identificacaoms/IdentificacaomsApplication.java
@@ -0,0 +1,13 @@
+package br.com.haiyu.identificacaoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentificacaomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentificacaomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/haiyu/identificacaoms/IdentificacaomsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.haiyu.identificacaoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentificacaomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
