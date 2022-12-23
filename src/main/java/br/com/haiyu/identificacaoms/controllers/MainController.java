6b86d579018e61ea0880af4bc6c0064a70a9fb29
Marcola <marcosguimaraest04@gmail.com>
2022-12-23 15:25:26 -0300

    Controller principal criado

---
+++ src/main/java/br/com/haiyu/identificacaoms/controllers/MainController.java
@@ -0,0 +1,10 @@
+package br.com.haiyu.identificacaoms.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	
+}
